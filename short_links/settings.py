fd38c4d5e0019aca690f5d91975cb189fd0c4747
khlova <74211951+khlova@users.noreply.github.com>
2021-02-25 22:19:43 +0300

    settings.py

--- short_links/settings.py
+++ short_links/settings.py
@@ -94,13 +94,12 @@ USE_TZ = True
 
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_root")
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/pictures/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
